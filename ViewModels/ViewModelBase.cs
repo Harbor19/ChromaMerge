506e4af7ccb8be48f810e18f12dbed4270567d02
結友 <miagetegorann@gmail.com>
2026-01-15 19:58:33 +0900

    プロジェクトファイルを追加。

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ChromaMerge.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ChromaMerge.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
